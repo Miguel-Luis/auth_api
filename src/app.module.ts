f16ea6bb7b209acc44f5a217b9569faf9330dc1c
Migue <miguel.gonzalez@cinnco.co>
2025-02-10 12:11:28 -0500

    Configuración de Swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { PrimaService } from './prima/prima.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [AppController],
-  providers: [AppService, PrimaService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
